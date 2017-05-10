a35f6ba7c10475fdc4e8a25c9b244f0a7c87ce6b
toby-loan <bigsilversstar@yandex.com>
2017-05-10 18:56:39 +0200

    Github.io

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Big-Silver.github.io
+Portal for IBM open source at GitHub http://ibm.github.io.
+
+Visit us at https://hub.jazz.net/project/kellrman/IBMGitHubOrg.
+
+[Contact us](mailto:hub@jazz.net)
---
+++ orgs.js
@@ -0,0 +1,150 @@
+
+    var orgs = [
+        {
+            "name": "Angular2 & crud-cli",
+            "type": "project",
+            "language": "AngularJS",
+            "description": "This project was generated with angular-cli version 1.0.0-beta.26.",
+            "html_url": "https://github.com/Big-Silver/Angular2-crud-cli"
+        }, 
+        {
+            "name": "Angular2 & Google-Map",
+            "type": "project",
+            "language": "AngularJS",
+            "description": "This project was generated with angular-cli version 1.0.0-beta.26. Using Google Map Api, Manage the serious informations of Google Map Marker.",
+            "html_url": "https://github.com/Big-Silver/Angular2-Google-Map"
+        },
+        {
+            "name": "Node-Authentication",
+            "type": "project",
+            "language": "Node/Express, MongoDB",
+            "description": "User SignUp & LogIn, Reset via Email, implemented the Authentication",
+            "html_url": "https://github.com/Big-Silver/Node-Authentication"
+        },
+        {
+            "name": "Angular & CKEditor-Drag n Drop",
+            "type": "project",
+            "language": "AngularJS, CKEDitor",
+            "description": "This project was generated with angular-cli version 1.0.0-beta.26. Using Angular+CKEditor+Drag and Drop, Edit the Basic Template.",
+            "html_url": "https://github.com/Big-Silver/Angular-Drag-and-Drop"
+        },
+        {
+            "name": "D3-Mobile-Suits",
+            "type": "project",
+            "language": "D3.js, Javascript",
+            "description": "This project is Mobile-Patent-Suits using D3.js",
+            "html_url": "https://github.com/Big-Silver/D3-Mobile-Suits"
+        },
+        {
+            "name": "D3-Click-Zoom",
+            "type": "project",
+            "language": "D3.js, Javascript",
+            "description": "This project is Mobile-Patent-Suits using D3.js",
+            "html_url": "https://github.com/Big-Silver/D3-Click-Zoom"
+        },
+        {
+            "name": "Angular2-Dashboard-Firebase",
+            "type": "project",
+            "language": "AngualrJS, Firebase",
+            "description": "This project was generated with angular-cli version 1.0.0-beta.28.3. Using Angular2, Theme and Firebase, create the Angular2-Dashboard-Firebase seed project.",
+            "html_url": "https://github.com/Big-Silver/Angular2-Dashboard-Firebase"
+        },
+        {
+            "name": "React-D3-Click",
+            "type": "project",
+            "language": "React.js, D3.js",
+            "description": "This project generates data using an interactive graph using React.js and D3.js",
+            "html_url": "https://github.com/Big-Silver/React-D3-Click"
+        },
+        {
+            "name": "React-Dashboard",
+            "type": "project",
+            "language": "React.js",
+            "description": "This project is Mobile-Patent-Suits using D3.js",
+            "html_url": "https://github.com/Big-Silver/React-Dashboard"
+        },
+        {
+            "name": "Ionic-GoogleMap",
+            "type": "project",
+            "language": "Ionic Framework, Google Map SDK",
+            "description": "Using using Google Maps Native SDK v2 and Ionic Framework, create the Ionic-Google_Map project.",
+            "html_url": "https://github.com/Big-Silver/Ionic-GoogleMap"
+        },
+        {
+            "name": "JQuery-Drag n Drop",
+            "type": "project",
+            "language": "JQuery, Javascript",
+            "description": "Using Jquery, Html, CSS and Javascript, implement the D & D(drag and drop), clone, resize, rotate and remove.",
+            "html_url": "https://github.com/Big-Silver/Drag-and-Drop"
+        },
+        {
+            "name": "Angular2-Calendar",
+            "type": "project",
+            "language": "AngularJS",
+            "description": "A calendar component for angular 2.0+ that can display events on a month, week or day view.",
+            "html_url": "https://github.com/Big-Silver/Angular2-Calendar"
+        },
+        {
+            "name": "React-Multistep",
+            "type": "project",
+            "language": "React.js, React-Multistep",
+            "description": "MultiStep Pages and User SignUp & LogIn",
+            "html_url": "https://github.com/Big-Silver/React-Multistep"
+        },
+        {
+            "name": "Drag n Drop & FileUpload",
+            "type": "project",
+            "language": "Dropzone.js",
+            "description": "This project is the Drag and Drop Fileupload using Dropzone.js.",
+            "html_url": "https://github.com/Big-Silver/Drag-n-Drop-Fileupload"
+        },
+        {
+            "name": "React-Drag_Sort",
+            "type": "project",
+            "language": "React.js, Redux",
+            "description": "Using React.js, React-Dom and React-sort, React-Drag_Sort is built.",
+            "html_url": "https://github.com/Big-Silver/React-Drag_Sort"
+        },
+        {
+            "name": "Auto-Run",
+            "type": "project",
+            "language": "Javascript, JQuery",
+            "description": "This project is Mobile-Patent-Suits using D3.js",
+            "html_url": "https://github.com/Big-Silver/D3-Mobile-Suits"
+        },
+        {
+            "name": "React/Redux & Socketio-Chat",
+            "type": "project",
+            "language": "React.js, Redux",
+            "description": "When URL is changed, the video or action run automatically regarding the content of page.",
+            "html_url": "https://github.com/Big-Silver/React-Redux-Socketio-Chat"
+        },
+        {
+            "name": "Simple Node-RESTful-API",
+            "type": "project",
+            "language": "D3.js, Javascript",
+            "description": "Simple CRUD RESTFul API using Node-Express.",
+            "html_url": "https://github.com/Big-Silver/Node-RESTful-API"
+        },
+        {
+            "name": "Redux-SoundCloud",
+            "type": "project",
+            "language": "React.js, Redux, SoundCloud API",
+            "description": "This is the Music Single Application using React/Redux and SoundCloud API",
+            "html_url": "https://github.com/Big-Silver/Redux-SoundCloud"
+        },
+        {
+            "name": "Redux-Climate-Dashboard",
+            "type": "project",
+            "language": "React.js, Redux",
+            "description": "Material and Graphic Design, Climate-Dashboard using React/Redux.",
+            "html_url": "https://github.com/Big-Silver/Redux-Climate-Dashboard"
+        },
+        {
+            "name": "Fabric-Drag n Drop",
+            "type": "project",
+            "language": "Fabric.js, Javascript, JQuery",
+            "description": "This project is Drag n Drop, Clone, Resize, Rotate and Delete using Fabric.js, Javascript and JQuery.",
+            "html_url": "https://github.com/Big-Silver/Fabric-Drag-n-Drop"
+        },
+    ];
